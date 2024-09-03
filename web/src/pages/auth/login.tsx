9440217b046227fc0ca19f40c00d36ac7d85c8ec
Joao Rangel <joaohcrangel@gmail.com>
2024-09-03 13:49:12 -0300

    fix login loyout

--- web/src/pages/auth/login.tsx
+++ web/src/pages/auth/login.tsx
@@ -1,11 +1,9 @@
 import { Card } from '@/components/ui/card'
-import { BlockView } from '@/features/views'
 import { UserAuthForm } from './components/user-auth-form'
 
 export default function SignIn2() {
   return (
     <>
-      <BlockView />
       <div className='container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0'>
         <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
           <div className='mb-4 flex items-center justify-center'>
@@ -31,7 +29,7 @@ export default function SignIn2() {
           <Card className='p-6'>
             <div className='flex flex-col space-y-2 text-left'>
               <h1 className='mb-4 text-2xl font-semibold tracking-tight'>
-                Authentication
+                Autenticação
               </h1>
             </div>
             <UserAuthForm />
