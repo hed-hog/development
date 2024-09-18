7d388e049649a5fabb67f49634a2e21004296107
Joao Rangel <joaohcrangel@gmail.com>
2024-09-17 22:48:27 -0300

    update backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MenuModule } from '@hedhog/menu';
+import { ScreenModule } from '@hedhog/screen';
+import { PaginationModule } from '@hedhog/pagination';
 import { AuthModule } from '@hedhog/auth';
+import { MailModule } from '@hedhog/mail';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerModule } from '@nestjs/throttler';
@@ -7,9 +11,21 @@ import { PrismaModule } from '@hedhog/prisma';
 
 @Module({
   imports: [
-    AuthModule,
     PrismaModule,
     ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]),
+    MailModule.forRoot({
+      global: true,
+      type: 'SMTP',
+      host: 'changeme',
+      port: 465,
+      secure: false,
+      username: 'changeme',
+      password: 'changeme',
+    }),
+    AuthModule,
+    PaginationModule,
+    ScreenModule,
+    MenuModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/typeorm/migrations/1726623572707-migrate.ts
@@ -0,0 +1,91 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+import { idColumn, timestampColumn } from "@hedhog/utils";
+
+export class Migrate1726623572707 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "menus",
+        columns: [
+          idColumn(),
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "url",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "order",
+            type: "int",
+            default: 0,
+            unsigned: true,
+          },
+          {
+            name: "menu_id",
+            type: "int",
+          },
+          {
+            name: "icon",
+            type: "varchar",
+            isNullable: true,
+          },
+          timestampColumn(),
+          timestampColumn("updated_at"),
+        ],
+      }),
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: "menu_screens",
+        columns: [
+          {
+            name: "menu_id",
+            type: "int",
+            isPrimary: true,
+            unsigned: true,
+          },
+          {
+            name: "screen_id",
+            type: "int",
+            isPrimary: true,
+            unsigned: true,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      "menu_screens",
+      new TableForeignKey({
+        columnNames: ["menu_id"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "menus",
+        onDelete: "CASCADE",
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      "menu_screens",
+      new TableForeignKey({
+        columnNames: ["screen_id"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "screens",
+        onDelete: "CASCADE",
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("menus");
+    await queryRunner.dropTable("menu_screens");
+  }
+}
