cc4ad163602dc030b059bb7417539ba963691034
Joao Rangel <joaohcrangel@gmail.com>
2024-10-07 13:32:49 -0300

    ok

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,26 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AdminModule } from '@hedhog/admin';
-import { MailModule } from '@hedhog/mail';
 import { PaginationModule } from '@hedhog/pagination';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '@hedhog/prisma';
 
 @Module({
-  imports: [
-    PrismaModule,
-    PaginationModule,
-    MailModule.forRoot({
-      global: true,
-      type: 'SMTP',
-      host: 'changeme',
-      port: 465,
-      secure: false,
-      username: 'changeme',
-      password: 'changeme',
-    }),
-    AdminModule,
-  ],
+  imports: [PrismaModule, PaginationModule],
   controllers: [AppController],
   providers: [AppService],
 })
