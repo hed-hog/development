a95cfdca7e09ce3eb410abf3ef8364fae941472f
Gabriel Lima <gabriellpbiel@hotmail.com>
2024-10-09 15:57:19 -0300

    toast translations pt2

--- admin/src/features/routes/api/handlers.ts
+++ admin/src/features/routes/api/handlers.ts
@@ -2,83 +2,99 @@ import { useMutation } from '@tanstack/react-query'
 import { requests } from './requests'
 import { queryClient } from '@/lib/query-provider'
 import { toast } from 'sonner'
+import { useTranslation } from 'react-i18next'
 
 export function useCreateRoute() {
   const { createRoute } = requests()
+  const { t: moduleT } = useTranslation('module')
+  const { t: successT } = useTranslation('success')
+  const { t: errorT } = useTranslation('error')
 
   return useMutation({
     mutationKey: ['post-route'],
     mutationFn: createRoute,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['routes'] })
-      toast.success('Route created successfully!')
+      toast.success(`${moduleT('route')} ${successT('create')}`)
     },
     onError: (error: any) => {
-      toast.error('Error creating route: ' + error.message)
+      toast.error(`${errorT('create')} ${moduleT('route')}` + error.message)
     },
   })
 }
 
 export function useEditRoute() {
   const { editRoute } = requests()
+  const { t: moduleT } = useTranslation('module')
+  const { t: successT } = useTranslation('success')
+  const { t: errorT } = useTranslation('error')
 
   return useMutation({
     mutationKey: ['edit-route'],
     mutationFn: editRoute,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['routes'] })
-      toast.success('Route edited successfully!')
+      toast.success(`${moduleT('route')} ${successT('edit')}`)
     },
     onError: (error: any) => {
-      toast.error('Error updating route: ' + error.message)
+      toast.error(`${errorT('edit')} ${moduleT('route')}` + error.message)
     },
   })
 }
 
 export function useDeleteRoute<T>() {
   const { deleteRoutes } = requests()
+  const { t: moduleT } = useTranslation('module')
+  const { t: successT } = useTranslation('success')
+  const { t: errorT } = useTranslation('error')
 
   return useMutation({
     mutationKey: ['delete-route'],
     mutationFn: deleteRoutes<T>,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['routes'] })
-      toast.success('Routes deleted successfully!')
+      toast.success(`${moduleT('route')} ${successT('delete')}`)
     },
     onError: (error: any) => {
-      toast.error('Error deleting Routes: ' + error.message)
+      toast.error(`${errorT('delete')} ${moduleT('route')}` + error.message)
     },
   })
 }
 
 export function useEditRouteScreens() {
   const { editRouteScreens } = requests()
+  const { t: moduleT } = useTranslation('module')
+  const { t: successT } = useTranslation('success')
+  const { t: errorT } = useTranslation('error')
 
   return useMutation({
     mutationKey: ['edit-route-screens'],
     mutationFn: editRouteScreens,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['routes'] })
-      toast.success('Route screens edited successfully!')
+      toast.success(`${moduleT('routeScreen')} ${successT('edit')}`)
     },
     onError: (error: any) => {
-      toast.error('Error updating route screens: ' + error.message)
+      toast.error(`${errorT('edit')} ${moduleT('routeScreen')}` + error.message)
     },
   })
 }
 
 export function useEditRouteRoles() {
   const { editRouteRoles } = requests()
+  const { t: moduleT } = useTranslation('module')
+  const { t: successT } = useTranslation('success')
+  const { t: errorT } = useTranslation('error')
 
   return useMutation({
     mutationKey: ['edit-route-roles'],
     mutationFn: editRouteRoles,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['routes'] })
-      toast.success('Route roles edited successfully!')
+      toast.success(`${moduleT('routeRole')} ${successT('edit')}`)
     },
     onError: (error: any) => {
-      toast.error('Error updating route roles: ' + error.message)
+      toast.error(`${errorT('edit')} ${moduleT('routeRole')}` + error.message)
     },
   })
 }
--- admin/src/features/settings/api/handlers.ts
+++ admin/src/features/settings/api/handlers.ts
@@ -2,51 +2,61 @@ import { useMutation } from '@tanstack/react-query'
 import { requests } from './requests'
 import { queryClient } from '@/lib/query-provider'
 import { toast } from 'sonner'
+import { useTranslation } from 'react-i18next'
 
 export function useCreateSetting() {
   const { createSetting } = requests()
+  const { t: moduleT } = useTranslation('module')
+  const { t: successT } = useTranslation('success')
+  const { t: errorT } = useTranslation('error')
 
   return useMutation({
     mutationKey: ['post-setting'],
     mutationFn: createSetting,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['settings'] })
-      toast.success('setting created successfully!')
+      toast.success(`${moduleT('setting')} ${successT('create')}`)
     },
     onError: (error: any) => {
-      toast.error('Error creating setting: ' + error.message)
+      toast.error(`${errorT('create')} ${moduleT('setting')}` + error.message)
     },
   })
 }
 
 export function useDeleteSettings<T>() {
   const { deleteSettings } = requests()
+  const { t: moduleT } = useTranslation('module')
+  const { t: successT } = useTranslation('success')
+  const { t: errorT } = useTranslation('error')
 
   return useMutation({
     mutationKey: ['delete-setting'],
     mutationFn: deleteSettings<T>,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['settings'] })
-      toast.success('Settings deleted successfully!')
+      toast.success(`${moduleT('setting')} ${successT('delete')}`)
     },
     onError: (error: any) => {
-      toast.error('Error deleting settings: ' + error.message)
+      toast.error(`${errorT('delete')} ${moduleT('setting')}` + error.message)
     },
   })
 }
 
 export function useEditSetting() {
   const { editSetting } = requests()
+  const { t: moduleT } = useTranslation('module')
+  const { t: successT } = useTranslation('success')
+  const { t: errorT } = useTranslation('error')
 
   return useMutation({
     mutationKey: ['edit-setting'],
     mutationFn: editSetting,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['settings'] })
-      toast.success('Setting edited successfully!')
+      toast.success(`${moduleT('setting')} ${successT('edit')}`)
     },
     onError: (error: any) => {
-      toast.error('Error updating setting: ' + error.message)
+      toast.error(`${errorT('edit')} ${moduleT('setting')}` + error.message)
     },
   })
 }
