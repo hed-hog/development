c562c68ca2a42444c06053daec7ec3ea66618140
Joao Rangel <joaohcrangel@gmail.com>
2024-10-29 00:00:51 -0300

    ok

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
+import { PersonModule } from '@hedhog/person';
+import { FileModule } from '@hedhog/file';
+import { AdminModule } from '@hedhog/admin';
+import { PaginationModule } from '@hedhog/pagination';
+import { MailModule } from '@hedhog/mail';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { PrismaModule } from '@hedhog/prisma';
 
 @Module({
-  imports: [PrismaModule, ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }])],
+  imports: [
+    PrismaModule,
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]),
+    MailModule.forRoot({
+      global: true,
+      type: 'SMTP',
+      host: 'changeme',
+      port: 465,
+      secure: false,
+      username: 'changeme',
+      password: 'changeme',
+    }),
+    PaginationModule,
+    AdminModule,
+    FileModule,
+    PersonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
