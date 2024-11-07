0e4ca42c55a05d6f8d89f3c735158e05cae6651c
Gabriel Lima <gabriellpbiel@hotmail.com>
2024-11-07 16:27:44 -0300

    feat: atualizar dependência @hedhog/utils para 0.0.23 e remover arquivos de localização obsoletos

--- lib/libs/person/src/person.module.ts
+++ lib/libs/person/src/person.module.ts
@@ -1,12 +1,12 @@
-import { PersonCustomModule } from './person-custom/person-custom.module';
+import { PersonCustomModule } from './person/person-custom/person-custom.module';
 import { PersonCustomTypeModule } from './person-custom-type/person-custom-type.module';
-import { PersonAddressModule } from './person-address/person-address.module';
+import { PersonAddressModule } from './person/person-address/person-address.module';
 import { PersonAddressTypeModule } from './person-address-type/person-address-type.module';
-import { PersonContactModule } from './person-contact/person-contact.module';
+import { PersonContactModule } from './person/person-contact/person-contact.module';
 import { PersonContactTypeModule } from './person-contact-type/person-contact-type.module';
-import { PersonDocumentModule } from './person-document/person-document.module';
+import { PersonDocumentModule } from './person/person-document/person-document.module';
 import { PersonDocumentTypeModule } from './person-document-type/person-document-type.module';
-import { PersonModule } from './person/person.module';
+import { PersonSubModule } from './person/person.module';
 import { PersonTypeModule } from './person-type/person-type.module';
 import { AdminModule } from '@hedhog/admin';
 import { PaginationModule } from '@hedhog/pagination';
@@ -18,7 +18,7 @@ import { forwardRef, Module } from '@nestjs/common';
     forwardRef(() => PrismaModule),
     forwardRef(() => PaginationModule),
     forwardRef(() => PersonTypeModule),
-    forwardRef(() => PersonModule),
+    forwardRef(() => PersonSubModule),
     forwardRef(() => PersonDocumentTypeModule),
     forwardRef(() => PersonDocumentModule),
     forwardRef(() => PersonContactTypeModule),
@@ -27,6 +27,7 @@ import { forwardRef, Module } from '@nestjs/common';
     forwardRef(() => PersonAddressModule),
     forwardRef(() => PersonCustomTypeModule),
     forwardRef(() => PersonCustomModule),
+    forwardRef(() => PersonModule),
   ],
   controllers: [],
   providers: [],
