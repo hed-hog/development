6fc26c54f81821d56ba7e7434313001cab646f28
Joao Rangel <joaohcrangel@gmail.com>
2024-11-19 14:56:34 -0300

    feat: update @hedhog/utils and @hedhog/locale package versions to 0.0.24 and 0.0.19 respectively

--- scripts/update-hedhog-deps.ts
+++ scripts/update-hedhog-deps.ts
@@ -1,58 +1,59 @@
 import { readdir } from 'fs/promises';
 import { join } from 'path';
 import { run } from './../lib/scripts/run';
+import { existsSync } from 'fs';
 
 async function getDirectoriesPackages() {
-    const folders = ['lib']
+  const folders = ['lib'];
 
-    for (const libDir of await readdir('./lib/libs')) {
-        folders.push(`lib/libs/${libDir}`)
-    }
+  for (const libDir of await readdir('./lib/libs')) {
+    folders.push(`lib/libs/${libDir}`);
+  }
 
-    return folders
+  return folders;
 }
 
 async function updateHedhogDeps(path: string) {
-    console.info(`Updating ${path}...`);
-    const packagePath = join(__dirname, '..', path, 'package.json');
-    console.info(`Reading ${packagePath}...`);
+  console.info(`Updating ${path}...`);
+  const packagePath = join(__dirname, '..', path, 'package.json');
+  console.info(`Reading ${packagePath}...`);
+
+  if (existsSync(packagePath)) {
     const packageJson = require(packagePath);
 
-    const {dependencies = {}, devDependencies = {}} = packageJson;
+    const { dependencies = {}, devDependencies = {} } = packageJson;
 
-    const devDeps = Object.keys(devDependencies).filter(dep => dep.split('/')[0] === '@hedhog').map((dep) => `${dep}@latest`)
-    const deps = Object.keys(dependencies).filter(dep => dep.split('/')[0] === '@hedhog').map((dep) => `${dep}@latest`)
+    const devDeps = Object.keys(devDependencies)
+      .filter((dep) => dep.split('/')[0] === '@hedhog')
+      .map((dep) => `${dep}@latest`);
+    const deps = Object.keys(dependencies)
+      .filter((dep) => dep.split('/')[0] === '@hedhog')
+      .map((dep) => `${dep}@latest`);
 
     if (devDeps.length) {
+      console.info(`Installing devDependencies: ${devDeps}`);
 
-        console.info(`Installing devDependencies: ${devDeps}`);
-
-        await run(path, 'npm', 'i', '-D', ...devDeps, '--legacy-peer-deps');
-
+      await run(path, 'npm', 'i', '-D', ...devDeps, '--legacy-peer-deps');
     }
 
     if (deps.length) {
+      console.info(`Installing deps: ${deps}`);
 
-        console.info(`Installing deps: ${deps}`);
-
-        await run(path, 'npm', 'i', ...deps, '--legacy-peer-deps');
-
+      await run(path, 'npm', 'i', ...deps, '--legacy-peer-deps');
     }
 
     console.info(`Updated!`);
+  }
 }
 
 async function main() {
-    
-    const folders = await getDirectoriesPackages();
-    
-    for (const folder of folders) {
-        await updateHedhogDeps(folder)
-    }
+  const folders = await getDirectoriesPackages();
 
+  for (const folder of folders) {
+    await updateHedhogDeps(folder);
+  }
 }
 
 main().catch((err) => {
-    console.error('Error:', err);
+  console.error('Error:', err);
 });
-  
