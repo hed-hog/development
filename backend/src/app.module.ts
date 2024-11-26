573aafaee826742883a71ae0cd42728c7a7bca64
Gabriel Lima <gabriellpbiel@hotmail.com>
2024-11-26 11:00:23 -0300

    chore: update dependencies for contact and locale modules; add country module integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ContactModule } from '@hedhog/contact';
 import { FileModule } from '@hedhog/file';
+import { CountryModule } from '@hedhog/country';
 import { AdminModule } from '@hedhog/admin';
 import { SettingModule } from '@hedhog/setting';
 import { MailModule } from '@hedhog/mail';
@@ -28,6 +29,7 @@ import { PrismaModule } from '@hedhog/prisma';
     }),
     SettingModule,
     AdminModule,
+    CountryModule,
     FileModule,
     ContactModule,
   ],
