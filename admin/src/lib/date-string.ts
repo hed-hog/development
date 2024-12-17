4ffab6b319cedfe417fdb2657af213162300c635
Gabriel Lima <gabriellpbiel@hotmail.com>
2024-12-17 08:55:40 -0300

    feat: validar string de data antes de conversão para objeto Date

--- admin/src/lib/date-string.ts
+++ admin/src/lib/date-string.ts
@@ -1,4 +1,8 @@
 export const isValidDateString = (value: any) => {
+  if (typeof value !== 'string') return false
+  const dateRegex = /^\d{4}-\d{2}-\d{2}$/
+  if (!dateRegex.test(value)) return false
+
   const date = new Date(value)
   return !isNaN(date.getTime())
 }
