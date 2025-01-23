93872fcdefc45f413ea054570855ae952613909a
saulotarsobc <saulotarsobc@gmail.com>
2025-01-23 16:29:52 -0300

    :fire: update cbc

--- lib/libs/cbc/src/global-metric/dto/create.dto.ts
+++ lib/libs/cbc/src/global-metric/dto/create.dto.ts
@@ -1,15 +1,27 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class CreateDTO {
+  @IsOptional()
   @IsNumber()
-  btc_dominance: number;
+  btc_dominance?: number;
 
+  @IsOptional()
   @IsNumber()
-  btc_dominance_24h_percentage_change: number;
+  btc_dominance_24h_percentage_change?: number;
 
+  @IsOptional()
   @IsNumber()
-  total_market_cap_yesterday_percentage_change: number;
+  total_market_cap_yesterday_percentage_change?: number;
 
+  @IsOptional()
   @IsNumber()
-  total_market_cap: number;
+  total_market_cap?: number;
+
+  @IsOptional()
+  @IsNumber()
+  btc_future?: number;
+
+  @IsOptional()
+  @IsNumber()
+  btc_future_change?: number;
 }
