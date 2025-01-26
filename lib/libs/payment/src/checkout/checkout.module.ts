eff77cb13178ab1248ecc371c9111e136bd46010
Joao Rangel <joaohcrangel@gmail.com>
2025-01-25 22:54:05 -0300

    feat: add HttpModule and integrate MercadoPagoProvider with axios for payment methods retrieval

--- lib/libs/payment/src/checkout/checkout.module.ts
+++ lib/libs/payment/src/checkout/checkout.module.ts
@@ -1,12 +1,14 @@
 import { AdminModule } from '@hedhog/admin';
 import { PaginationModule } from '@hedhog/pagination';
 import { PrismaModule } from '@hedhog/prisma';
+import { HttpModule } from '@nestjs/axios';
 import { forwardRef, Module } from '@nestjs/common';
 import { CheckoutController } from './checkout.controller';
 import { CheckoutService } from './checkout.service';
 
 @Module({
   imports: [
+    HttpModule,
     forwardRef(() => AdminModule),
     forwardRef(() => PrismaModule),
     forwardRef(() => PaginationModule),
--- lib/libs/payment/src/checkout/provider/mercado-pago.provider.ts
+++ lib/libs/payment/src/checkout/provider/mercado-pago.provider.ts
@@ -1,7 +1,15 @@
+import { HttpService } from '@nestjs/axios';
+import { BadRequestException } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 import { AbstractProvider } from './abstract,provider';
 
 export class MercadoPagoProvider extends AbstractProvider {
-  constructor(private setting: Record<string, string>) {
+  private baseUrl = 'https://api.mercadopago.com';
+
+  constructor(
+    private setting: Record<string, string>,
+    private readonly httpService: HttpService,
+  ) {
     super();
   }
 
@@ -11,5 +19,25 @@ export class MercadoPagoProvider extends AbstractProvider {
 
   async createSubscription(priceId: string, customerId: string): Promise<any> {
     return { priceId, customerId, setting: this.setting };
+  }
+
+  async getPaymentMethods() {
+    if (!this.setting['payment-mercado-pago-token']) {
+      throw new BadRequestException(
+        `You must set the storage provider in the setting.`,
+      );
+    }
+
+    const { data } = await lastValueFrom(
+      this.httpService.request({
+        url: `${this.baseUrl}/v1/payment_methods`,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.setting['payment-mercado-pago-token']}`,
+        },
+      }),
+    );
+
+    return data;
   }
 }
