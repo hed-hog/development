4327fda8367c99bd6726540915f494d31b3dddb1
Gabriel Lima <gabriellpbiel@hotmail.com>
2025-02-05 14:52:34 -0300

    feat: atualizar versão do pacote @hedhog/admin para 0.46.23 e adicionar suporte à configuração de URL da aplicação

--- lib/libs/admin/src/admin.module.ts
+++ lib/libs/admin/src/admin.module.ts
@@ -1,17 +1,19 @@
+import { LocaleModule } from '@hedhog/locale';
+import { MailModule } from '@hedhog/mail';
 import { PaginationModule } from '@hedhog/pagination';
 import { PrismaModule } from '@hedhog/prisma';
 import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { LocaleModule } from '@hedhog/locale';
 import { MenuModule } from './menu/menu.module';
 import { RoleModule } from './role/role.module';
 import { RouteModule } from './route/route.module';
 import { ScreenModule } from './screen/screen.module';
 import { UserModule } from './user/user.module';
-import { MailModule } from '@hedhog/mail';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     forwardRef(() => AuthModule),
     forwardRef(() => MailModule),
     forwardRef(() => MenuModule),
