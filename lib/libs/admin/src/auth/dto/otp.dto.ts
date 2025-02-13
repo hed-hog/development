242beab8a3215a66adf8d89a533a92c2bd5605ea
saulotarsobc <saulotarsobc@gmail.com>
2025-02-13 09:34:39 -0300

    feat: adicionar mensagens de validação personalizadas nos DTOs de autenticação

--- lib/libs/admin/src/auth/dto/otp.dto.ts
+++ lib/libs/admin/src/auth/dto/otp.dto.ts
@@ -1,11 +1,11 @@
 import { IsInt, IsJWT, Max, Min } from 'class-validator';
 
 export class OtpDTO {
-  @Min(0)
-  @Max(999999)
-  @IsInt()
+  @Min(0, { message: 'O código deve ser pelo menos 0.' })
+  @Max(999999, { message: 'O código deve ser no máximo 999999.' })
+  @IsInt({ message: 'O código deve ser um número inteiro.' })
   code: number;
 
-  @IsJWT()
+  @IsJWT({ message: 'Token inválido' })
   token: string;
 }
--- lib/libs/admin/src/auth/dto/reset.dto.ts
+++ lib/libs/admin/src/auth/dto/reset.dto.ts
@@ -1,14 +1,16 @@
 import { IsNotEmpty, MinLength } from 'class-validator';
 
 export class ResetDTO {
-    @IsNotEmpty()
-    @MinLength(8)
-    newPassword: string;
+  @IsNotEmpty({ message: 'Por favor, preencha a nova senha.' })
+  @MinLength(8, { message: 'A nova senha deve ter pelo menos 8 caracteres.' })
+  newPassword: string;
 
-    @IsNotEmpty()
-    @MinLength(8)
-    confirmNewPassword: string;
+  @IsNotEmpty({ message: 'Por favor, confirme a nova senha.' })
+  @MinLength(8, {
+    message: 'A confirmação da senha deve ter pelo menos 8 caracteres.',
+  })
+  confirmNewPassword: string;
 
-    @IsNotEmpty()
-    code: string;
+  @IsNotEmpty({ message: 'Por favor, informe o código.' })
+  code: string;
 }
