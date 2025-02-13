fcf5107a7e366adff510090b5774c0be740452f5
saulotarsobc <saulotarsobc@gmail.com>
2025-02-13 11:43:55 -0300

    chore: adicionar pacotes @nestjs/core, @nestjs/platform-express e class-transformer ao projeto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,8 @@
+import { PrismaModule } from '@hedhog/prisma';
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { PrismaModule } from '@hedhog/prisma';
 
 @Module({
   imports: [PrismaModule, ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }])],
