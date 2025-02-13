568ab9af1d1c5f61a3246ae51faa7c13758095f5
saulotarsobc <saulotarsobc@gmail.com>
2025-02-13 09:36:02 -0300

    feat: adicionar mensagens de validação personalizadas nos DTOs de exclusão e atualização de IDs

--- lib/libs/admin/src/dto/delete.dto.ts
+++ lib/libs/admin/src/dto/delete.dto.ts
@@ -1,8 +1,8 @@
 import { ArrayMinSize, IsArray, IsInt } from 'class-validator';
 
 export class DeleteDTO {
-  @IsArray()
-  @ArrayMinSize(1)
-  @IsInt({ each: true })
+  @IsArray({ message: 'Por favor, forneça uma lista de itens.' })
+  @ArrayMinSize(1, { message: 'A lista de itens não pode estar vazia.' })
+  @IsInt({ each: true, message: 'Todos os itens devem ser números inteiros.' })
   ids: number[];
 }
--- lib/libs/admin/src/dto/update-ids.dto.ts
+++ lib/libs/admin/src/dto/update-ids.dto.ts
@@ -3,7 +3,10 @@ import { IsArray, IsInt } from 'class-validator';
 export class UpdateIdsDTO {
   @IsInt({
     each: true,
+    message: 'Todos os itens devem ser numéricos.',
   })
-  @IsArray()
+  @IsArray({
+    message: 'Por favor, forneça uma lista de itens.',
+  })
   ids: number[];
 }
