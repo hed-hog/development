666873f07ba632557f49f56d7d47c1a0a701ed73
Joao Rangel <joaohcrangel@gmail.com>
2025-02-14 16:38:56 -0300

    feat: update profile package version to 0.0.24 and simplify InitDTO validation

--- lib/libs/payment/src/checkout/dto/init.dto.ts
+++ lib/libs/payment/src/checkout/dto/init.dto.ts
@@ -1,23 +1,14 @@
-import {
-  ArrayMinSize,
-  ArrayNotEmpty,
-  IsArray,
-  IsInt,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsArray, IsInt, IsOptional, IsString } from 'class-validator';
 
 export class InitDTO {
+  @IsOptional()
   @IsString({
     message: 'Por favor, forneça um identificador válido para o slug.',
   })
   slug: string;
 
+  @IsOptional()
   @IsArray({ message: 'A lista de itens deve ser uma lista.' })
-  @ArrayNotEmpty({ message: 'A lista de itens não pode estar vazia.' })
-  @ArrayMinSize(1, {
-    message: 'A lista de itens deve conter pelo menos um item.',
-  })
   @IsInt({
     each: true,
     message: 'Cada item na lista deve ser um número inteiro.',
