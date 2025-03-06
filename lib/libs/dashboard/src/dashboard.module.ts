a7db28c3cf98cf535cd3a888e106bee1cac49c40
Gabriel Lima <gabriellpbiel@hotmail.com>
2025-03-06 14:22:00 -0300

    feat: atualizar versão do módulo @hedhog/dashboard para 0.0.2; adicionar módulo Prisma e ajustar dependências

--- lib/libs/dashboard/src/dashboard.module.ts
+++ lib/libs/dashboard/src/dashboard.module.ts
@@ -1,9 +1,11 @@
+import { LocaleModule } from '@hedhog/locale';
+import { PrismaModule } from '@hedhog/prisma';
 import { forwardRef, Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
-import { LocaleModule } from '@hedhog/locale';
+
 @Module({
-  imports: [forwardRef(() => LocaleModule)],
+  imports: [forwardRef(() => LocaleModule), forwardRef(() => PrismaModule)],
   controllers: [DashboardController],
   providers: [DashboardService],
   exports: [forwardRef(() => DashboardService)],
