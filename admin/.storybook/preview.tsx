27c330a7bab2abd1461b322eb80ee0047dbda66b
Gabriel Lima <gabriellpbiel@hotmail.com>
2025-03-27 15:31:01 -0300

    feat: adicionar suporte a formulários no Storybook com react-hook-form

--- admin/.storybook/preview.tsx
+++ admin/.storybook/preview.tsx
@@ -5,6 +5,8 @@ import { themes } from 'storybook/internal/theming'
 import { ThemeProvider } from '../src/components/app/theme-provider'
 import { Toaster } from '../src/components/ui/toaster'
 import { AppProvider } from '../src/lib/app-provider'
+import { useForm } from 'react-hook-form'
+import { Form } from '../src/components/ui/form'
 
 const preview: Preview = {
   parameters: {
@@ -20,10 +22,14 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => {
+      const methods = useForm()
+
       return (
         <AppProvider>
           <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-            <Story />
+            <Form {...methods}>
+              <Story />
+            </Form>
             <Toaster />
           </ThemeProvider>
         </AppProvider>
