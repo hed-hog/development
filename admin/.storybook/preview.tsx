5852a0aace37234541b7d8f8f460f0c24d650f6f
saulotarsobc <saulotarsobc@gmail.com>
2025-03-27 13:47:36 -0300

    feat: reorganize imports in Storybook preview configuration

--- admin/.storybook/preview.tsx
+++ admin/.storybook/preview.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import type { Preview } from '@storybook/react'
 import '@/index.css'
-import { AppProvider } from '../src/lib/app-provider'
+import type { Preview } from '@storybook/react'
+import React from 'react'
+import { themes } from 'storybook/internal/theming'
 import { ThemeProvider } from '../src/components/theme-provider'
 import { Toaster } from '../src/components/ui/toaster'
-import { themes } from 'storybook/internal/theming'
+import { AppProvider } from '../src/lib/app-provider'
 
 const preview: Preview = {
   parameters: {
