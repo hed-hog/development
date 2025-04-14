64744ac1bd8e5968833c20f9b9c33c2df6908e03
Gabriel Lima <gabriellpbiel@hotmail.com>
2025-04-14 15:28:59 -0300

    Atualiza versão do pacote @hedhog/mail-manager para 0.48.27 e adiciona o módulo MailSentModule às importações do MailModule.

--- lib/libs/mail-manager/src/mail/mail.module.ts
+++ lib/libs/mail-manager/src/mail/mail.module.ts
@@ -3,6 +3,7 @@ import { MailModule as MailMainModule } from '@hedhog/mail';
 import { PaginationModule } from '@hedhog/pagination';
 import { PrismaModule } from '@hedhog/prisma';
 import { forwardRef, Module } from '@nestjs/common';
+import { MailSentModule } from '../mail-sent/mail-sent.module';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
 
@@ -10,6 +11,7 @@ import { MailService } from './mail.service';
   imports: [
     forwardRef(() => AdminModule),
     forwardRef(() => MailMainModule),
+    forwardRef(() => MailSentModule),
     forwardRef(() => PrismaModule),
     forwardRef(() => PaginationModule),
   ],
