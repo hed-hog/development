7368d80ca8763cc1d2b1608a4f83a9a61743d6b7
Gabriel Lima <gabriellpbiel@hotmail.com>
2025-10-15 16:38:58 -0300

    fix: payment list

--- lib/libs/payment/src/item/item.consts.ts
+++ lib/libs/payment/src/item/item.consts.ts
@@ -1,17 +1,15 @@
-import { Prisma } from "@prisma/client";
-
-export const includeItem: Prisma.itemInclude = {
-    payment_coupon_item: {
+export const includeItem: any = {
+  payment_coupon_item: {
+    select: {
+      id: true,
+      item_id: true,
+      coupon_id: true,
+      payment_coupon: {
         select: {
-            id: true,
-            item_id: true,
-            coupon_id: true,
-            payment_coupon: {
-                select: {
-                    id: true,
-                    code: true,
-                },
-            },
+          id: true,
+          code: true,
         },
+      },
     },
-}
+  },
+};
--- lib/libs/payment/src/payment-coupon/payment-coupon.consts.ts
+++ lib/libs/payment/src/payment-coupon/payment-coupon.consts.ts
@@ -1,18 +1,16 @@
-import { Prisma } from "@prisma/client";
-
-export const includePaymentCoupon: Prisma.payment_couponInclude = {
-    payment_coupon_item: {
+export const includePaymentCoupon: any = {
+  payment_coupon_item: {
+    select: {
+      id: true,
+      item_id: true,
+      item: {
         select: {
-            id: true,
-            item_id: true,
-            item: {
-                select: {
-                    id: true,
-                    name: true,
-                    price: true,
-                    slug: true,
-                },
-            },
+          id: true,
+          name: true,
+          price: true,
+          slug: true,
         },
+      },
     },
-}
+  },
+};
